ea393f0433ad5268687c218ecbf488080be82f29
Amir <12301688@students.koi.edu.au>
2025-01-16 03:00:58 +0500

    Update default email and add question duration

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  # default from: "info@students.koi.edu.au"
+  default from: "info@students.koi.edu.au"
   layout "mailer"
 end
